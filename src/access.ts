8f9e95d1d0e86aed9001a1c310838458971b5713
Cheems <1040461147@qq.com>
2024-04-09 11:45:10 +0800

    feat: 重写系统管理（之前）

--- src/access.ts
+++ src/access.ts
@@ -1,12 +1,14 @@
-import { TGetMenusByUserIdRes } from './services'
+// import { TGetMenusByUserIdRes } from './services'
 
 /**
  * @see https://umijs.org/docs/max/access#access
  * */
-export default function access(initialState: { userMenus?: TGetMenusByUserIdRes } | undefined) {
-  const userMenus = initialState?.userMenus ?? []
+// export default function access(initialState: { userMenus?: TGetMenusByUserIdRes } | undefined) {
+export default function access() {
+  // const userMenus = initialState?.userMenus ?? []
 
   return {
-    normalRouteFilter: (route: any) => userMenus.find((item) => item.name === route.name),
+    // normalRouteFilter: (route: any) => userMenus.find((item) => item.name === route.name),
+    normalRouteFilter: true,
   }
 }
